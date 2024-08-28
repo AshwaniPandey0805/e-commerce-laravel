da85689cfa771100ca0c107ecb1a54addae1e07b
Ashwani Kumar Pandey <ashwanipandey679@gmail.com>
2024-08-28 20:22:39 +0530

    added product on home page

--- e_commerce/app/Helpers/helper.php
+++ e_commerce/app/Helpers/helper.php
@@ -1,3 +1,12 @@
-<?php 
-    // publuc function getSubc
+<?php
+
+use App\Models\Category;
+
+    function getCategories(){
+        return Category::orderBy('name', 'ASC')
+                        ->where('show_home', 'Yes')
+                        ->where('status', 1)
+                        ->with('getSubCategories')
+                        ->get();
+    }
 ?>
--- e_commerce/app/Http/Controllers/FrontController.php
+++ e_commerce/app/Http/Controllers/FrontController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
     public function index(){
-        return view('front.home');
+        $featuredProducts = Product::where('is_featured', 'Yes')
+                            ->where('status' , 1)
+                            ->orderBy('title', 'ASC')
+                            ->with('product_images')
+                            ->get();
+        $latestProduct = Product::where('status', 1)
+                                ->orderBy('id','ASC')
+                                ->with('product_images')
+                                ->get();
+        $data['featuredProducts'] = $featuredProducts; 
+        $data['latestProduct'] = $latestProduct; 
+        return view('front.home', $data);
     }
 }
+
